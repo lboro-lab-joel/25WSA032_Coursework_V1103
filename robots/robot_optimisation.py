561383a1b8c109065546e02d2bb01bd124954494
Joel Kurian <530917.lboro@gmail.com>
2026-04-29 15:54:17 +0100

    Added baseline and optimised configuration in file (may be edited in future commits)

--- robots/robot_optimisation.py
+++ robots/robot_optimisation.py
@@ -12,6 +12,11 @@ damage - damage taken by bot
 from robots.ecosystem.factory import ecofactory
 from robots.ecosystem.ecosystem import distance
 
+DROIDS = 3
+DRONES = 4
+ROBOTS = 4
+CHARGERS = ([55, 20], [30, 40], [70, 30])
+PIZZAS = 10
 
 import matplotlib as plt
 plt.close("all")
@@ -21,9 +26,25 @@ plt.ion()
 optimum_radius = 6
 charge_threshold = 30
 
-setting = ecofactory(robots = 3, droids = 4, drones = 5, chargers = ([55, 20], [30, 40], [70, 30]), pizzas  = 10)
+setting = ecofactory(ROBOTS, DRONES, DROIDS, CHARGERS, PIZZAS)
 
 charger = setting.chargers()[0]
+
+BASELINE_CONFIG = {
+    "charger" : [[40, 20]],
+    "charge_threshold" : 0.20,
+    "max_pizza_weight" : 20,
+    "close_pizza" : False
+}
+
+OPTIMISED_CONFIG = {
+    "charger" : [[40, 20], [30, 50], [60, 70]],
+    "charge_threshold" : 0.35,
+    "max_pizza_weight" : 50,
+    "close_pizza" : False
+}
+
+
 
 def low_battery():
     for bot in setting.bots():
