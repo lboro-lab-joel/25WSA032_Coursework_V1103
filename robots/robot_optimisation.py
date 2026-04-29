b15ab446ed362e0707e67f0a7180894079816382
Joel Kurian <530917.lboro@gmail.com>
2026-04-29 22:09:13 +0100

    Added constant variables I think i need for the project, may update if needed. changed "setting" to es for ease of typing

--- robots/robot_optimisation.py
+++ robots/robot_optimisation.py
@@ -11,45 +11,66 @@ damage - damage taken by bot
 
 from robots.ecosystem.factory import ecofactory
 from robots.ecosystem.ecosystem import distance
+import matplotlib.pyplot as plt
 
 DROIDS = 3
 DRONES = 4
 ROBOTS = 4
-CHARGERS = ([55, 20], [30, 40], [70, 30])
 PIZZAS = 10
 
-import matplotlib as plt
-plt.close("all")
-plt.ion()
+DURATION = "1 week"
+
+BASELINE_CHARGERS = [[40,20]]
+OPTIMISED_CHARGERS = [[20, 10], [60, 10], [40, 35]]
+
+HOME = [40, 20, 0]
+
+
+BASELINE_THRESHOLD = 0.2
+
+OPTIMISED_THRESHOLDS = {
+   "Robot": 0.28, 
+   "Droid": 0.22,
+   "Drone": 0.30
+}
+
+OPPORTUNISTIC_RADIUS = 6
+OPPORTUNISTIC_SOC = 0.6
+
+BASELINE_MAX_WEIGHT = 20
+OPTIMISED_MAX_WEIGHT = 40
+
+PIZZA_POOL_SIZE = 15
 
 
 optimum_radius = 6
 charge_threshold = 30
 
-setting = ecofactory(ROBOTS, DRONES, DROIDS, CHARGERS, PIZZAS)
 
-charger = setting.chargers()[0]
-
-BASELINE_CONFIG = {
-    "charger" : [[40, 20]],
-    "charge_threshold" : 0.20,
-    "max_pizza_weight" : 20,
-    "close_pizza" : False
-}
-
-OPTIMISED_CONFIG = {
-    "charger" : [[40, 20], [30, 50], [60, 70]],
-    "charge_threshold" : 0.35,
-    "max_pizza_weight" : 50,
-    "close_pizza" : False
-}
+def create_deliverables(es, target = PIZZA_POOL_SIZE):
+  
+  '''
+  This function tries to create enough pizzas in the case they run out.
+  '''
+  ready_count = sum(1 for p in es.deliverables() if p.status == "ready") # Counts the number of pizzas that are available
+  for _ in range(max(0, target- ready_count)): # Way to create missing pizzas
+    es.create_thing("Pizza")
 
 
 
-def low_battery():
-    for bot in setting.bots():
-        if bot.soc/ bot.maxsoc < charge_threshold and bot.station == None:
-            bot.charge(charger)
+
+
+
+plt.close("all")
+plt.ion()
+
+
+es = ecofactory(robots = ROBOTS, drones = DRONES, droids = DROIDS, chargers = CHARGERS, pizzas = PIZZAS)
+
+while es.active:
+
+  for bot in es.bots():
+    pass
 
 
 
