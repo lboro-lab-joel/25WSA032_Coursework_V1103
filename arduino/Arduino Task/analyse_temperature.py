2d37b6790844c830e0e377c3ecbee4d671bd467e
Joel Kurian <530917.lboro@gmail.com>
2026-05-14 14:56:48 +0100

    Added Code to show the graph between the temperature against time. The other graphs will have simmilar formats with different values.

--- arduino/Arduino Task/analyse_temperature.py
+++ arduino/Arduino Task/analyse_temperature.py
@@ -16,3 +16,15 @@ magnitude = df["Magnitude"].to_numpy(dtype = float)
 
 DATA_FILE = "temperature_data.csv"
 MA_WINDOW = 10 # window size
+
+#This is the code to fit the differnet graphs
+fig, axes = plt.subplots(3, 2, figsize=(14, 12))
+fig.suptitle("Arduino Temperature Monitoring — Data Analysis", fontsize=14)
+
+# Temperature against Time
+ax = axes[0, 0]
+ax.plot(time, temperature, color = "steelblue")
+ax.set_title("Plot 1: Temperature vs Time")
+ax.set_xlabel("Time(s)")
+ax.set_ylabel("Temperature (*C)")
+ax.grid(True, linestyle = "--", alpha = 0.5)
